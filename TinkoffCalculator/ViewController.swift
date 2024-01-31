acfed4e5396bd9512d023df32e4d3aee59445af9
VWalker <kaneuus@gmail.com>
2024-01-31 19:56:54 +0300

    Started developing

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Dmitrii on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("started!")
+    }
+
+
+}
+
