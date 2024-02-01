be718cd2a93c4cd89c68a6217927b8f67b611b06
VWalkerFPS <kaneuus@gmail.com>
2024-02-01 22:43:48 +0300

    Added buttons

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else { return }
+        print(buttonTitle)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("APP LAUNCHED")
         
-        print("started!")
     }
-
-
 }
 
